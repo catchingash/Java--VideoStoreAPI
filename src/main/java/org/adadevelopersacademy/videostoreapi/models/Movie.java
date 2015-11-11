1a463aabeecc7e40493b2966a4a296794428cf68
Ashley Watkins <AWatkins31@gmail.com>
2015-11-10 16:37:58 -0800

    Creates Movie model.

---
+++ src/main/java/org/adadevelopersacademy/videostoreapi/models/Movie.java
@@ -0,0 +1,23 @@
+package org.adadevelopersacademy.videostoreapi.models;
+
+public class Movie {
+    private final int id;
+    private String title;
+    private String overview;
+    private String releaseDate;
+    private int inventory;
+
+    public Movie(
+            int id,
+            String title,
+            String overview,
+            String releaseDate,
+            int inventory
+    ) {
+        this.id = id;
+        this.title = title;
+        this.overview = overview;
+        this.releaseDate = releaseDate;
+        this.inventory = inventory;
+    }
+}
