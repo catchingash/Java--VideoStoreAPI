3a849883a64d0b8a52a4a020e64c47ec561a593a
Ashley Watkins <AWatkins31@gmail.com>
2015-11-10 16:43:49 -0800

    Minor refactor: Shifts format of HelloController.

--- src/main/java/org/adadevelopersacademy/videostoreapi/controllers/HelloController.java
+++ src/main/java/org/adadevelopersacademy/videostoreapi/controllers/HelloController.java
@@ -4,24 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class HelloController {
-
     @RequestMapping("/foo/{name}")
-	public String printWelcome(ModelMap model, @PathVariable String name, @RequestParam(required=false, defaultValue="") String age) {
+    public String printWelcome(
+            ModelMap model,
+            @PathVariable String name,
+            @RequestParam(required = false, defaultValue = "") String age
+    ) {
+        model.addAttribute("message", name + age);
+        return "hello";
+    }
 
-		model.addAttribute("message", name + age);
-		return "hello";
-	}
-
-	@RequestMapping("/foo/get2")
-	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "It worked");
-		return "hello";
-	}
-
+    @RequestMapping("/foo/get2")
+    public String printWelcome(
+            ModelMap model
+    ) {
+        model.addAttribute("message", "It worked");
+        return "hello";
+    }
 }
