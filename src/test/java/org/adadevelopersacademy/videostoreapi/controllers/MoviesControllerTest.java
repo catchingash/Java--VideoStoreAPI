8db1c00a2122669efae3bfc680cd9efe0f290d98
Ashley Watkins <AWatkins31@gmail.com>
2015-11-17 12:35:04 -0800

    Adds a quick setup of a controller test.

---
+++ src/test/java/org/adadevelopersacademy/videostoreapi/controllers/MoviesControllerTest.java
@@ -0,0 +1,12 @@
+package org.adadevelopersacademy.videostoreapi.controllers;
+
+import org.junit.Test;
+
+public class MoviesControllerTest {
+    @Test
+    public void thisIsWhatAControllerUnitTestWouldLookLike() {
+//        MoviesController moviesController = new MoviesController();
+//        moviesController.show("Title");
+//        // test something!
+    }
+}
